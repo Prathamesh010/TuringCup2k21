4c5d3e9ce6dd90b3380fcd296c8ca735c985756e
Prathamesh010 <prathameshgawas87@gmail.com>
2021-03-15 12:24:27 +0530

    navbar added

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express = require('express')
+const express = require("express")
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.set('view engine', 'pug')
+app.set("view engine", "pug")
+app.use(express.static(__dirname + "/static"))
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get("/", (req, res) => {
+  res.render("index")
 })
 
 app.listen(PORT, (err) => {
-    if(err) console.log(err)
-    console.log(`Server Listening on Port ${PORT}...`)
+  if (err) console.log(err)
+  console.log(`Server Listening on Port ${PORT}...`)
 })
