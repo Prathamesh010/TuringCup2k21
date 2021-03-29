cf083ed938ea533fdf51612d5c58931d3b0f494c
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-03-29 18:59:02 +0530

    Resolve redirect issue

--- routes/formRoute.js
+++ routes/formRoute.js
@@ -35,7 +35,7 @@ router.post("/", upload.single("upload"), async (req, res) => {
         imageUrl: url,
       })
       await newResponse.save()
-      res.redirect("/registrationSuccessfull")
+      res.redirect("/")
     })
     .catch((err) => {
       res.status(200).json({
