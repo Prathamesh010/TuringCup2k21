3a107c6217ac7fd81bce53e75ba8c6d9b483fed4
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-03-30 17:04:55 +0530

    Dump unnecessary files

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require("express")
-const mongoose = require("mongoose")
-const formRoute = require("./routes/formRoute")
 
 const app = express()
 
@@ -9,25 +7,9 @@ const PORT = process.env.PORT || 3000
 app.set("view engine", "pug")
 app.use(express.static(__dirname + "/static"))
 
-const uri =
-  "mongodb+srv://Samprit:samprit@cluster0.ziqcu.mongodb.net/mySecondDatabase?retryWrites=true&w=majority"
-mongoose.connect(
-  uri,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err) => {
-    if (err) console.log(err)
-  }
-)
-
 app.get("/", (req, res) => {
   res.render("index")
 })
-
-app.get('/regsuccess', (req, res) => {
-  res.render('regSuccessful')
-})
-
-app.use("/form", formRoute)
 
 app.listen(PORT, (err) => {
   if (err) console.log(err)
